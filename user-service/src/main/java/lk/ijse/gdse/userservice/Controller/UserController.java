0d0ed27f0b555bbc7ba5d11acc7c01da057ea03e
Nethpali_Jayaweera <nethpalijayaweera@gmail.com>
2024-07-02 10:24:12 +0530

    User Save Done

--- user-service/src/main/java/lk/ijse/gdse/userservice/Controller/UserController.java
+++ user-service/src/main/java/lk/ijse/gdse/userservice/Controller/UserController.java
@@ -1,8 +1,11 @@
 package lk.ijse.gdse.userservice.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.gdse.userservice.Dto.UserDTO;
+import lk.ijse.gdse.userservice.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Nethpali Jayaweera
@@ -10,8 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+    @Autowired
+    private UserService  userService;
+
     @GetMapping("/health")
     public String healthCheck() {
         return "User service is up and running";
     }
+
+
+    @PostMapping(value = "/save",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> save(@RequestBody UserDTO userDTO) {
+        userService.save(userDTO);
+        return ResponseEntity.ok("User saved successfully");
+    }
 }
+
+
